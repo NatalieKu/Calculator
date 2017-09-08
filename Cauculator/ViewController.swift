dc53e9697fae00ad1532688a5f7a5fa5b3f0189f
MEI KU <natalie@MEIde-MacBook-Air.local>
2017-09-08 17:46:44 +0800

    initial

--- Cauculator/ViewController.swift
+++ Cauculator/ViewController.swift
@@ -10,6 +10,105 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var numbersOnScreen:Double = 0
+    var previousNumber:Double = 0
+    var performingMath = false
+    var operation = 0
+    
+    @IBOutlet weak var clear: UILabel!
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func numbersButton(_ sender: UIButton) {
+        clear.isHidden = true
+      if performingMath == true {
+            label.text = String(sender.tag-1)
+            numbersOnScreen = Double(label.text!)!
+            performingMath = false
+        }
+        else {
+        label.text = label.text! + String(sender.tag-1)
+        numbersOnScreen = Double(label.text!)!
+        }
+    }
+
+    @IBAction func calculateButton(_ sender: UIButton) {
+        clear.isHidden = true
+        if label.text != "" && sender.tag != 16 {
+            
+     previousNumber = Double(label.text!)!
+            if sender.tag == 12
+            {
+                label.text = "/"
+            }
+            else if sender.tag == 13 {
+                 label.text = "*"
+            }
+                
+            else if sender.tag == 14 {
+                label.text = "-"
+
+            }
+
+            else if sender.tag == 14 {
+                label.text = "+"
+            }
+            
+            else if sender.tag == 17 {
+                
+                label.text = String(Double(label.text!)! / 100 )
+                
+            }
+            
+            else if sender.tag == 11 {
+            
+                label.text =  String(-Double(label.text!)!)
+            
+            }
+            
+      
+            
+            
+     operation = sender.tag
+    performingMath = true
+        }
+       else if sender.tag == 16 {
+            if operation == 12 {
+                label.text = String(previousNumber / numbersOnScreen)
+            }
+           else if operation == 13 {
+                label.text = String(previousNumber * numbersOnScreen)
+            }
+            else if operation == 14 {
+                label.text = String(previousNumber - numbersOnScreen)
+            }
+            if operation == 15 {
+                label.text = String(previousNumber + numbersOnScreen)
+            }
+            
+            if operation == 15 {
+                label.text = String(previousNumber + numbersOnScreen)
+            }
+
+            
+        }
+        
+        
+    }
+
+    
+    @IBAction func clearButton(_ sender: UIButton) {
+ 
+       label.text = ""
+        clear.isHidden = false
+        
+    
+    }
+    
+
+    
+
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
